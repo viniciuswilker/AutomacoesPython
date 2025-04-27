371d17d4cafcc1c205219d852029b1ccfe60e795
Vinícius Wilker <159863481+viniciuswilker@users.noreply.github.com>
2025-04-27 18:54:10 -0300

    .

--- teste.py
+++ teste.py
@@ -1,8 +1,42 @@
+#  INSTALANDO MANUAL
+
+# from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service
+
+# service = Service(executable_path="./chromedriver.exe")
+
+# driver = webdriver.Chrome(service=service)
+
+# driver.get('https://www.youtube.com')
+
+
+#  ==================================
+#  INSTALAR O WEBDRIVER AUTO
+
+#  pip install webdriver-manager
+
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
-service = Service(executable_path="./chromedriver.exe")
-
-driver = webdriver.Chrome(service=service)
+# Ele baixa e gerencia automaticamente o chromedriver correto
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 driver.get('https://www.youtube.com')
+
+
+
+
+
+# ============================
+
+#  MAIS DIRETO AO PONTO ( MENOS PROF )
+
+import webbrowser
+
+# URL que você quer abrir
+url = "https://cursoautomacao.netlify.app"
+
+# Abrir o site no navegador padrão
+webbrowser.open(url)
