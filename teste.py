d1c253d6c06794c39cb5c7fbd82fc3633eb9b5a4
Vinícius Wilker <159863481+viniciuswilker@users.noreply.github.com>
2025-04-27 18:30:04 -0300

    arq

---
+++ teste.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+service = Service(executable_path="./chromedriver.exe")
+
+driver = webdriver.Chrome(service=service)
+
+driver.get('https://www.youtube.com')
